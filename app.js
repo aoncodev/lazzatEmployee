aef33cad69bd4f7f6bf2778793978e8bb2f09cf8
aoncodev <aoncodev@gmail.com>
2025-01-02 23:29:05 +0500

    add cors

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://3.36.115.48", // Allow only your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", require("./routes/employee"));
